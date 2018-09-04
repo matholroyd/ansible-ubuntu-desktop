abe427ad6600ecf66f4732f12750eff63e8ee5b7
Mat Holroyd <contact@matholroyd.com>
2018-09-04 18:05:54 +1000

    WIP

--- files/bash_profile.sh
+++
@@ -1,9 +0,0 @@
-export GOROOT=/usr/local/go
-export GOPATH=$GOROOT/bin
-
-export PATH=$HOME/.rbenv/bin:$PATH
-export PATH=$GOPATH:$PATH
-# export PATH=/usr/local/nodejs/bin:$PATH
-
-# rbenv initialization
-eval "$(rbenv init -)"
